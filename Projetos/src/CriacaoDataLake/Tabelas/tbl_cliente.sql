0d4b61ab8d0c870048419eabc90bf944998752df
Software Developer <software.developer.br@gmail.com>
2023-09-20 18:57:41 -0400

    Projeto 3 - version 02

---
+++ Projetos/src/CriacaoDataLake/Tabelas/tbl_cliente.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[tbl_cliente]
+(
+	 cod_cliente VARCHAR(25) NOT NULL PRIMARY KEY
+	,desc_cliente VARCHAR(200) NULL
+	,endereco VARCHAR(200) NULL
+	,faturamento FLOAT
+	,desc_segmento VARCHAR(200)
+	,desc_cidade VARCHAR(200)
+	,cod_estado VARCHAR(25)
+	,desc_estado VARCHAR(200)
+	,desc_regiao VARCHAR(200)
+	,arquivo_origem VARCHAR(200)
+	,data_carga DATETIME
+)
---
+++ Projetos/src/CriacaoDataLake/Tabelas/tbl_produto.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tbl_produto]
+(
+	 cod_produto VARCHAR(25) NOT NULL PRIMARY KEY
+	,desc_produto VARCHAR(200) NULL
+	,desc_departamento VARCHAR(200) NULL
+	,atr_unidade_medida VARCHAR(50) NULL
+	,cod_fornecedor VARCHAR(25) null
+	,desc_fornecedor VARCHAR(200)
+	,arquivo_origem VARCHAR(200)
+	,data_carga DATETIME
+)
